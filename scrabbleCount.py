b0c232f46a5eba1dbb393fba286f94fd7839d520
Carl 'Subzidion' Hiltbrunner <carlhiltbrunner@hotmail.com>
2016-06-20 21:17:39 -0700

    Made the output prettier

--- scrabbleCount.py
+++ scrabbleCount.py
@@ -1,15 +1,15 @@
-count = { 'a': 9, 'b': 2, 'c': 2, 'd': 4, 'e': 12, 'f': 2, 'g': 3, 'h': 2, 'i': 9, 'j': 1, 'k': 1, 'l': 4, 'm': 2, 'n': 6, 'o': 8, 'p': 2, 'q': 1, 'r': 6, 's': 4, 't': 6, 'u': 4, 'v': 2, 'w': 2, 'x': 1, 'y': 2, 'z': 1, '_':2 }
+count = { 'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2, 'G': 3, 'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4, 'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6, 'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1, 'Y': 2, 'Z': 1, '_': 2 }
 
 currentCount = dict(count)
 
 board = input()
 
 for character in board:
-    characterCount = currentCount[character.lower()] - 1
+    characterCount = currentCount[character.upper()] - 1
     if characterCount < 0:
         print('Invalid input. More ' + character.upper() + '\'s have been taken from the bag than possible.')
         exit()
-    currentCount[character.lower()] = currentCount[character.lower()] - 1
+    currentCount[character.upper()] = currentCount[character.upper()] - 1
 
 def generateCount():
     for i in range(12, -1, -1):
@@ -18,6 +18,6 @@ def generateCount():
             if value is i:
                 ithCount.append(key)
         if len(ithCount) != 0:
-            print(str(i) + ': ' + '{}'.format(', '.join(sorted(ithCount))))
+            print(str(i).rjust(2) + ': ' + '{}'.format(', '.join(sorted(ithCount))))
 
 generateCount()
